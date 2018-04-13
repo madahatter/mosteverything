5280c888fe8f8ecef7c8f4930eaddadb0a71535a
madahatter <adamharper9@gmail.com>
2018-04-13 14:18:45 -0400

    test with simon

--- decode/03_javascript-fundamentals/15_inheritance/src/problem1.js
+++ decode/03_javascript-fundamentals/15_inheritance/src/problem1.js
@@ -26,12 +26,6 @@ class Square extends Rectangle{
         this.height = side
         this.width = side
     }
-    // A square is a rectangle
-    // Every square has a width and a height
-    // The height and width of a square are always the same
-    // Implement the constructor
-    // Do not implement the area and perimeter methods. They should be inherited from Rectangle
-    // The constructor has one argument
 }
 
 
